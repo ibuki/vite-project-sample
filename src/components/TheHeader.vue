3eda29f40de4d6595eacc272d8036d56f5f59611
Ibuki Suzuki <ibuki.nakamura@gmail.com>
2022-06-16 15:46:25 +0900

    Remove tailwindcss

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center space-x-3 mb-3">
+  <div>
     <RouterLink :to="{ name: 'home' }">Home</RouterLink>
     <RouterLink :to="{ name: 'page1' }">Page1</RouterLink>
   </div>
